baba0d6a5857e77136271fc04cd5658237209ff6
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-04-25 12:47:02 +0500

    Refactor CollectionItemRepository to use CollectionItemId as the identifier and update query method for clarity

--- src/main/java/kz/example/backend/virtualcollections/repository/CollectionItemRepository.java
+++ src/main/java/kz/example/backend/virtualcollections/repository/CollectionItemRepository.java
@@ -1,6 +1,7 @@
 package kz.example.backend.virtualcollections.repository;
 
 import kz.example.backend.virtualcollections.entity.CollectionItem;
+import kz.example.backend.virtualcollections.entity.CollectionItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,9 +10,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CollectionItemRepository extends JpaRepository<CollectionItem, Long> {
+public interface CollectionItemRepository extends JpaRepository<CollectionItem, CollectionItemId> {
 
     @Query("SELECT i FROM CollectionItem i WHERE i.collection.id = ?1")
-    Optional<List<CollectionItem>> getCollectionItemByUserId(Long id);
+    Optional<List<CollectionItem>> getCollectionItemByCollectionId(Long id);
 
 }
